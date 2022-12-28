2705f981bbdf693c50fae97574cb6985bbf5d03e
TithiB-del <76741863+TithiB-del@users.noreply.github.com>
2022-12-28 23:21:47 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,19 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _MyApp();
 }
-
-  
+class _MyApp extends State<MyApp> {
+  final List<Widget> _list = [ Text(“Event1”) ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appbar: Appbar(
+      title: Text(“Event Scheduler”),
+  ),
+        body: ListView.builder(
+          itemCount: _list.length,
+          itemBuilder: ((context,index) => _list[index])),
+        ),
+      ),
+    ),
+  }
