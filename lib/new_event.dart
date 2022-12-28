6c4045af045dedc8ea1febdf0079f9426f9c1024
TithiB-del <76741863+TithiB-del@users.noreply.github.com>
2022-12-29 00:20:17 +0530

    imported packages

--- lib/new_event.dart
+++ lib/new_event.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 class NewEventScreen extends State<NewEventScreen>{
   final TextEditingController _textEditingController = TextEditingController();
   @override
