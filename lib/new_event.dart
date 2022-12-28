52434704888c9f2e309dcec10d6caa93195741fc
TithiB-del <76741863+TithiB-del@users.noreply.github.com>
2022-12-29 00:13:44 +0530

    Update new_event.dart

--- lib/new_event.dart
+++ lib/new_event.dart
@@ -1,1 +1,18 @@
-
+class NewEventScreen extends State<NewEventScreen>{
+  final TextEditingController _textEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(
+      title: Text(“New Event”),
+    ),
+                    body: Column( children: [ 
+                      Textfield( 
+                        controller: _textEditingController, ), 
+                                             ElevatedButton(onPressed: () {
+                                               String newEventText = textEditingController.text;
+                                               Navigator.of(context)},
+                                                            child: Text(“ADD”)), 
+                    ],
+                 ),
+              ),
+  }
